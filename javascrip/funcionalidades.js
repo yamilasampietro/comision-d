bafc5fcde34704b7317d61e97c643f6d8ce83e41
yamilasampietro <yamilasampietro@hotmail.com>
2024-05-14 21:27:55 -0300

    funcionalidades.js

---
+++ javascrip/funcionalidades.js
@@ -0,0 +1,16 @@
+
+
+var Nombre = "Ana";
+var Apellido = "Cleta";
+
+console.log(Nombre + " " + Apellido);
+
+
+var num1 = 1;
+var num2 = 2;
+var suma = num1+num2;
+
+console.log(suma)
+
+var num3 = suma+3;
+console.log(num3)
